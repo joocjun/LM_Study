0e15b59210870e6dda49747bd43a749ce2f3b2e9
ampehta <68820979+ampehta@users.noreply.github.com>
2021-03-14 20:53:16 +0900

    Update BERT_Model.py

--- SON/BERT_Model.py
+++ SON/BERT_Model.py
@@ -7,5 +7,11 @@ class Bert(tf.keras.Model):
   def __init__(self,):
     super(Bert,self).__init__()
     
+    self.emb = EmbLayer()
+    self.enc_layers = [EncoderBlock(hidden_size, head_num, 0.1, 1e-12) 
+                       for _ in range(6)]
+  def call(self):
+    
+    
     
     
