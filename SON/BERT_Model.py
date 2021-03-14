3685dfcbc571080826413c6b2a8527ee65b14522
ampehta <68820979+ampehta@users.noreply.github.com>
2021-03-14 20:51:04 +0900

    Update BERT_Model.py

--- SON/BERT_Model.py
+++ SON/BERT_Model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from EncoderBlock_Transformer import EncoderBlock
 from MultiHeadAttention import MultiHeadAttention
+from Encoding import EmbLayer
 
 class Bert(tf.keras.Model):
   def __init__(self,):
